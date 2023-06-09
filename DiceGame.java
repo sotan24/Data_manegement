6e1bd1b51a48712b6e69377333950675cf99bf18
sotan24 <s1300078@u-aizu.ac.jp>
2023-06-09 12:20:42 +0900

    Dice game: Greeting added

--- DiceGame.java
+++ DiceGame.java
@@ -1,10 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 public class DiceGame{
     public static void main(String[] args) {
         Random random = new Random();
         int ran1 = random.nextInt(6)+1;
         int ran2 = random.nextInt(6)+1;
         int total;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scan.next();
+        System.out.println("Hello, " + name + "!");
+
+
         System.out.println("Rolling dice...");
         System.out.println("Die 1: " + ran1);
         System.out.println("Die 2: " + ran2); 
