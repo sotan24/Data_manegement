3a54b788c57edce0b3c6bc469859e1f71898776e
sotan24 <s1300078@u-aizu.ac.jp>
2023-06-09 12:45:03 +0900

    Won/lost message modified

--- DiceGame.java
+++ DiceGame.java
@@ -20,9 +20,9 @@ public class DiceGame{
 
         System.out.println("Total value: " + total);
         if(total>7){
-            System.out.println("You won");
+            System.out.println(name +" won");
         }else{
-            System.out.println("You lost");
+            System.out.println(name + " lost");
         }
     }
 }
